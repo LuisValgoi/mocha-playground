4a202dc985f76280fcdbb1ad69572b2783e6a672
Luis Valgoi <luis.valgoi@sap.com>
2019-09-13 13:31:42 -0300

    added user test class

---
+++ test/model/UserTest.js
@@ -0,0 +1,11 @@
+const assert = require("chai").assert;
+const sinon = require("sinon");
+const User = require("../../model/User");
+
+describe("User.js file", () => {
+  context("constructor", () => {
+    it("should construct NAME and LASTNAME as null", () => {
+      
+    });
+  });
+});
